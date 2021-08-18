8352b63b737cbd1940a6062ed0c7953771f857e9
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2021-08-18 11:27:54 +0300

    .

---
+++ QR(ver.2)/QR(ver.2)/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QR_ver._2_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Qr qr = new Qr();
+        ImageSource qrimage;
+        System.Drawing.Image file;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SaveQR(System.Drawing.Image bmp1)  //сохранить qr
+        {
+            SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "QR";
+            dlg.Filter = "PNG|*.png|JPEG|*.jpeg|GIF|*.gif|BMP|*.bmp";                                                    
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                string format = dlg.Filter;
+                switch (format)
+                {
+                    case "png":
+                        {
+                            bmp1.Save(filename, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                        }
+                    case "jpeg":
+                        {
+                            bmp1.Save(filename, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        }
+                    case "gif":
+                        {
+                            bmp1.Save(filename, System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                        }
+                    case "bmp":
+                        {
+                            bmp1.Save(filename, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                        }
+                }
+            }
+        }
+        public System.Drawing.Image openfile() //открыть файл
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            ofd.ShowDialog();
+            string filename = ofd.FileName; //////////
+            System.Drawing.Image newImage = System.Drawing.Image.FromFile(filename);
+
+            return newImage;
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            file = openfile();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveQR(qr.bmp);
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (file == null)
+            {
+                qrimage = qr.createQR(textBox.Text.Trim());
+                image.Source = qrimage;
+                textBox.Clear();
+            } else
+            {
+                qrimage = qr.createQR(textBox.Text.Trim(), file);
+                image.Source = qrimage;
+                textBox.Clear();
+            }
+            
+        }
+    }
+}
---
+++ QR(ver.2)/QR(ver.2)/Qr.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ZXing;
+using ZXing.QrCode;
+using System.Drawing;
+
+namespace QR_ver._2_
+{
+    class Qr
+    {
+        //поля
+        private int width, height;
+        BarcodeWriter qr;
+        public Bitmap bmp;
+
+        //конструктор
+        public Qr(int w = 300, int h = 300)
+        {
+            width = w;
+            height = h;
+            QrCodeEncodingOptions options = new QrCodeEncodingOptions { DisableECI = true, CharacterSet = "UTF-8", Width = width, Height = height };
+            qr = new BarcodeWriter();
+            qr.Options = options;
+            qr.Format = ZXing.BarcodeFormat.QR_CODE;
+        }
+        //методы
+        public ImageSource createQR(string TeXt)
+        {
+            Bitmap result = new Bitmap(qr.Write(TeXt));
+            bmp = result;
+            BitmapSource b = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(result.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            ImageSource IM = b;
+            return b;
+        }
+        public ImageSource createQR(string TeXt, Image overlay)
+        {
+            Bitmap result = new Bitmap(qr.Write(TeXt));
+            int deltaHeigth = height - overlay.Height;
+            int deltaWidth = width - overlay.Width;
+            Graphics g = Graphics.FromImage(bmp);//
+            g.DrawImage(overlay, new System.Drawing.Point(deltaWidth / 2, deltaHeigth / 2));
+            bmp = result;
+            BitmapSource b = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(result.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            ImageSource IM = b;
+            return b;
+        }
+
+    }
+}
