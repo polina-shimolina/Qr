6ccb2b5c3346f31c51b9f82506ebedc2f74247a8
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2021-09-21 11:43:49 +0300

    .
    картинка открывается теперь

--- QR(ver.2)/QR(ver.2)/Qr.cs
+++ QR(ver.2)/QR(ver.2)/Qr.cs
@@ -31,19 +31,17 @@ namespace QR_ver._2_
             Bitmap result = new Bitmap(qr.Write(TeXt));
             bmp = result;
             BitmapSource b = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(result.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-            ImageSource IM = b;
             return b;
         }
         public ImageSource createQR(string TeXt, Image overlay)
         {
             Bitmap result = new Bitmap(qr.Write(TeXt));
+            bmp = result;
             int deltaHeigth = height - overlay.Height;
             int deltaWidth = width - overlay.Width;
             Graphics g = Graphics.FromImage(bmp);//
             g.DrawImage(overlay, new System.Drawing.Point(deltaWidth / 2, deltaHeigth / 2));
-            bmp = result;
             BitmapSource b = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(result.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-            ImageSource IM = b;
             return b;
         }
 
